26a326e1eac23c4ed2e4fe1301010fe13b816234
Jooker188 <remydionisio@outlook.fr>
2022-03-18 16:18:47 +0100

    Init

---
+++ Player.java
@@ -0,0 +1,13 @@
+public class Player{
+    public int age;
+    public String nom;
+
+    public Player(int age, String nom){
+        this.age = age;
+        this.nom = nom;
+    }
+    public static void main(String args[]){
+        Player p = new Player(7, "rollo");
+        System.out.println("J'ai " + p.age + " ans\n" + "Et je m'appelle " + p.nom);
+    }
+}
